258c2862ea2898c01cc99d30a741e755bf4653bb
figojulioez <figojulioez@gmail.com>
2025-08-26 13:09:19 +0700

    Week 1

---
+++ Proyek 1/FigoProject/src/main/java/com/mycompany/figoproject/FigoProject.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.figoproject;
+
+/**
+ *
+ * @author VOSTRO V131
+ */
+public class FigoProject {
+
+    public static void main(String[] args) {
+        System.out.println("Nama     : Julioez Candita Haga Figo Latupeirissa");
+        System.out.println("NPM      : 2410506001");
+        System.out.println("Alamat   : Bekasi, Alamanda Regency Blok C2 Mp 9");
+        System.out.println("No_Hp    : 085215168961");
+
+
+    }
+}
